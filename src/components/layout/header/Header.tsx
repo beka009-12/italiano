20dd8b3c38c79bd80426956b8234a54fa7196fe0
nurkyzzz006 <nasnur399@gmail.com>
2025-09-01 17:28:18 +0600

    .

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,11 +1,25 @@
 import { type FC } from "react";
 import scss from "./Header.module.scss";
+import Link from "next/link";
+import { IoSearchOutline } from "react-icons/io5";
 
 const Header: FC = () => {
   return (
     <header id={scss.Header}>
       <div className="container">
-        <div className={scss.content}>Header</div>
+        <div className={scss.content}>
+          <h1>Restaurant</h1>
+          <div className={scss.nav}>
+            <Link href="#">Interior</Link>
+            <Link href="#">About Us</Link>
+            <Link href="#">Menu</Link>
+            <Link href="#">Contacts</Link>
+          </div>
+          <button>
+            <IoSearchOutline className={scss.search_icon} />
+            Search
+          </button>
+        </div>
       </div>
     </header>
   );
