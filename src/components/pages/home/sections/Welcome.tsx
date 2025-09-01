d68c234f6b3753eb62b9fddf1f1df497e36f9c8b
Arzubek1 <arzubek729@gmail.com>
2025-09-01 16:59:59 +0600

    .

--- src/components/pages/home/sections/Welcome.tsx
+++ src/components/pages/home/sections/Welcome.tsx
@@ -1,11 +1,19 @@
 import { type FC } from "react";
 import scss from "./Welcome.module.scss";
+import welcomeImage from "../../../../../public/wImage.jpg";
 
 const Welcome: FC = () => {
   return (
-    <section className={scss.Welcome}>
+    <section
+      className={scss.Welcome}
+      style={{
+        background: `url(${welcomeImage.blurDataURL})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center"
+      }}
+    >
       <div className="container">
-        <div className={scss.content}>Welcome hello</div>
+        <div className={scss.content}>
+          <h2>hello</h2>
+        </div>
       </div>
     </section>
   );
